d393f138475f6ac7e66149aa260a08668f6bf6ab
Puzzleleaf <cmtyx2@naver.com>
2020-06-28 21:14:33 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mnistdigitrecognizer/screens/draw_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mnist Digit Recognizer',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.black,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.black,
+        ),
+      ),
+      home: DrawScreen(),
+    );
+  }
+}
